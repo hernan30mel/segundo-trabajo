ba93007e3e100ddb0fee4352443078f8a025b807
hernan30mel <97269571+hernan30mel@users.noreply.github.com>
2022-01-09 21:57:37 -0500

    Add files via upload

---
+++ matri.S.java
@@ -0,0 +1,19 @@
+public class matri {
+    public static void main(String[] args) {
+        int[][] matriz = {{1, 1}, {1, 1}};
+        int total=0;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(matriz[i][j] + "\t");
+                total += matriz[i][j] ;
+            }
+            System.out.println("\n");
+        }
+
+        System.out.print(total);
+    }
+}
+
+
+
+
